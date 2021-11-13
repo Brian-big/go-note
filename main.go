4f790714f28cc184a91308b181b86d4cd863df9f
Brian-big <briannbig@gmail.com>
2021-11-13 12:15:57 +0300

    fetch notes endpoint

--- main.go
+++ main.go
@@ -6,63 +6,31 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-var db = make(map[string]string)
+type note struct {
+	ID      string `json:"id"`
+	Title   string `json:"title"`
+	Content string `json:"content"`
+}
 
+var notes = []note{
+	{"1", "start", "starting something is always important than never trying"},
+	{"2", "plan", "failing to plan is planning to fail"},
+	{"3", "get going", "no matter what happens, pursue your dreams to achieve your goal"},
+	{"4", "consistency", "always be consistent in what you do"},
+}
+
+func getAllNotes(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, notes)
+}
 func setupRouter() *gin.Engine {
-	// Disable Console Color
-	// gin.DisableConsoleColor()
+
 	r := gin.Default()
 
-	// Ping test
 	r.GET("/ping", func(c *gin.Context) {
 		c.String(http.StatusOK, "pong")
 	})
 
-	// Get user value
-	r.GET("/user/:name", func(c *gin.Context) {
-		user := c.Params.ByName("name")
-		value, ok := db[user]
-		if ok {
-			c.JSON(http.StatusOK, gin.H{"user": user, "value": value})
-		} else {
-			c.JSON(http.StatusOK, gin.H{"user": user, "status": "no value"})
-		}
-	})
-
-	// Authorized group (uses gin.BasicAuth() middleware)
-	// Same than:
-	// authorized := r.Group("/")
-	// authorized.Use(gin.BasicAuth(gin.Credentials{
-	//	  "foo":  "bar",
-	//	  "manu": "123",
-	//}))
-	authorized := r.Group("/", gin.BasicAuth(gin.Accounts{
-		"foo":  "bar", // user:foo password:bar
-		"manu": "123", // user:manu password:123
-	}))
-
-	/* example curl for /admin with basicauth header
-	   Zm9vOmJhcg== is base64("foo:bar")
-
-		curl -X POST \
-	  	http://localhost:8080/admin \
-	  	-H 'authorization: Basic Zm9vOmJhcg==' \
-	  	-H 'content-type: application/json' \
-	  	-d '{"value":"bar"}'
-	*/
-	authorized.POST("admin", func(c *gin.Context) {
-		user := c.MustGet(gin.AuthUserKey).(string)
-
-		// Parse JSON
-		var json struct {
-			Value string `json:"value" binding:"required"`
-		}
-
-		if c.Bind(&json) == nil {
-			db[user] = json.Value
-			c.JSON(http.StatusOK, gin.H{"status": "ok"})
-		}
-	})
+	r.GET("/notes", getAllNotes)
 
 	return r
 }
